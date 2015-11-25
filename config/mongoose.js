5dd40682ce1807497abb24f1520424b169fd0338
Marc S. Thomson <mthomson19@verizon.net>
2015-11-25 15:55:18 -0500

    Chapter 8 - CRUD Modules

--- config/mongoose.js
+++ config/mongoose.js
@@ -10,8 +10,9 @@ module.exports = function() {
 	// Use Mongoose to connect to MongoDB
 	var db = mongoose.connect(config.db);
 
-	// Load the 'User' model 
+	// Load the application models 
 	require('../app/models/user.server.model');
+	require('../app/models/article.server.model');
 
 	// Return the Mongoose connection instance
 	return db;
