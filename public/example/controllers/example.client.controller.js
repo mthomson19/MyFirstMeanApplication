1010c0f1859994e3b461df5b058c6c2b39ca48fe
Marc S. Thomson <mthomson19@verizon.net>
2015-11-25 16:09:49 -0500

    Chapter 8 - CRUD Modules

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -4,7 +4,7 @@
 // Create the 'example' controller
 angular.module('example').controller('ExampleController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
-		// Get the user's 'fullName' 
-		$scope.name = Authentication.user ? Authentication.user.fullName : 'MEAN Application';
+		// Expose the authentication service
+		$scope.authentication = Authentication;
 	}
 ]);
