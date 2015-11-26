b09979d112b19ec889a3c1f4c184254d1e679225
Marc S. Thomson <mthomson19@verizon.net>
2015-11-26 13:52:31 -0500

    Chapter 10 - Testing

--- config/env/development.js
+++ config/env/development.js
@@ -6,18 +6,18 @@ module.exports = {
 	db: 'mongodb://localhost/mean-development',
 	sessionSecret: 'developmentSessionSecret',
 	facebook: {
-		clientID: '1661927620755219',
-		clientSecret: '10459721ec278ba70e5ed2ee591994c0',
+		clientID: 'Facebook Application ID',
+		clientSecret: 'Facebook Application Secret',
 		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
 	},
 	twitter: {
-		clientID: 'onZbW6S2Thxheq5heh7YHqNS4',
-		clientSecret: '1OWv7l07rrB8ClthtfiL5a32xcPs7ouAMnEc67FChdCAaj1qDI',
+		clientID: 'Twitter Application ID',
+		clientSecret: 'Twitter Application Secret',
 		callbackURL: 'http://localhost:3000/oauth/twitter/callback'
 	},
 	google: {
-		clientID: '37251699804-t5espvj3dtqphfs8ph56imrlnnq6kmql.apps.googleusercontent.com',
-		clientSecret: 'CosE4wS9dJorvt8RQ37JVQOs',
+		clientID: 'Google Application ID',
+		clientSecret: 'Google Application Secret',
 		callbackURL: 'http://localhost:3000/oauth/google/callback'
 	}
 };
