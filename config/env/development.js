0ee0c7dcc426230d0fa1d9fdf3146ffebd30b46a
Marc S. Thomson <mthomson19@verizon.net>
2015-11-27 15:24:49 -0500

    Chapter 11 - Automation and Debugging

--- config/env/development.js
+++ config/env/development.js
@@ -6,18 +6,18 @@ module.exports = {
 	db: 'mongodb://localhost/mean-development',
 	sessionSecret: 'developmentSessionSecret',
 	facebook: {
-		clientID: 'Facebook Application ID',
-		clientSecret: 'Facebook Application Secret',
+		clientID: '1661927620755219',
+		clientSecret: '10459721ec278ba70e5ed2ee591994c0',
 		callbackURL: 'http://localhost:3000/oauth/facebook/callback'
 	},
 	twitter: {
-		clientID: 'Twitter Application ID',
-		clientSecret: 'Twitter Application Secret',
+		clientID: 'onZbW6S2Thxheq5heh7YHqNS4',
+		clientSecret: '1OWv7l07rrB8ClthtfiL5a32xcPs7ouAMnEc67FChdCAaj1qDI',
 		callbackURL: 'http://localhost:3000/oauth/twitter/callback'
 	},
 	google: {
-		clientID: 'Google Application ID',
-		clientSecret: 'Google Application Secret',
+		clientID: '37251699804-t5espvj3dtqphfs8ph56imrlnnq6kmql.apps.googleusercontent.com',
+		clientSecret: 'CosE4wS9dJorvt8RQ37JVQOs',
 		callbackURL: 'http://localhost:3000/oauth/google/callback'
 	}
 };
